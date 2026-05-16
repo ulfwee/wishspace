a75e4968b6b62f91ce6b5b8da6fa0c12c4e2595f
Uliana <ulfweey@gmail.com>
2026-05-16 19:04:34 +0300

    fixed for backend deployment

--- backend/src/app.js
+++ backend/src/app.js
@@ -11,7 +11,7 @@ const adminRoutes = require('./routes/admin');
 const app = express();
 
 app.use(cors({
-    origin: 'http://localhost:5173',
+    origin: 'https://wishspace-one.vercel.app/',
     credentials: true
 }));
 app.options('*', cors());
